d10d010dc31d6457946c11fc77fb23b689d7d83d
qiong <malatanggm@gmail.com>
2015-11-09 21:12:31 -0800

    Configurations

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+angular.module('blocitoff', ['firebase']);
+angular.module('blocitoff', ['ui.router']);
+
+angular.module('blocitoff').config(function($stateProvider, $locationProvider) {
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $stateProvider.state('main', {
+        url: '/main',
+        controller: 'Main.controller',
+        templateUrl: 'templates/main.html'
+    });
+    $stateProvider.state('devise', {
+        url: '/devise',
+        controller: 'Devise.controller',
+        templateUrl: 'templates/devise.html'
+    });
+    $stateProvider.state('layouts', {
+        url: '/layouts',
+        controller: 'Layouts.controller',
+        templateUrl: 'templates/layouts.html'
+    });
+    $stateProvider.state('lists', {
+        url: '/lists',
+        controller: 'Lists.controller',
+        templateUrl: 'templates/lists.html'
+    });
+    $stateProvider.state('tasks', {
+        url: '/tasks',
+        controller: 'Tasks.controller',
+        templateUrl: 'templates/tasks.html'
+    });
+});
