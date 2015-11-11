fb03e85035b6a611e1840bdc00e379b48b10eca8
qiong <malatanggm@gmail.com>
2015-11-11 10:48:18 -0800

    Added basic fn

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,34 +1,36 @@
-angular.module('blocitoff', ['firebase','ui.router']);
+angular.module('blocitoff', ['firebase', 'ui.router']);
 
-angular.module('blocitoff').config(function($stateProvider, $locationProvider) {
+angular.module('blocitoff')
+    .constant('FBURL', 'https://ng-blocitoff.firebaseio.com/')
+    .config(function ($stateProvider, $locationProvider) {
 
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+        $stateProvider.state('main', {
+            url: '/main',
+            controller: 'Main.controller',
+            templateUrl: 'templates/main.html'
+        });
+        $stateProvider.state('devise', {
+            url: '/devise',
+            controller: 'Devise.controller',
+            templateUrl: 'templates/devise.html'
+        });
+        $stateProvider.state('layouts', {
+            url: '/layouts',
+            controller: 'Layouts.controller',
+            templateUrl: 'templates/layouts.html'
+        });
+        $stateProvider.state('lists', {
+            url: '/lists',
+            controller: 'Lists.controller',
+            templateUrl: 'templates/lists.html'
+        });
+        $stateProvider.state('tasks', {
+            url: '/tasks',
+            controller: 'Tasks.controller',
+            templateUrl: 'templates/tasks.html'
+        });
     });
-    $stateProvider.state('main', {
-        url: '/main',
-        controller: 'Main.controller',
-        templateUrl: 'templates/main.html'
-    });
-    $stateProvider.state('devise', {
-        url: '/devise',
-        controller: 'Devise.controller',
-        templateUrl: 'templates/devise.html'
-    });
-    $stateProvider.state('layouts', {
-        url: '/layouts',
-        controller: 'Layouts.controller',
-        templateUrl: 'templates/layouts.html'
-    });
-    $stateProvider.state('lists', {
-        url: '/lists',
-        controller: 'Lists.controller',
-        templateUrl: 'templates/lists.html'
-    });
-    $stateProvider.state('tasks', {
-        url: '/tasks',
-        controller: 'Tasks.controller',
-        templateUrl: 'templates/tasks.html'
-    });
-});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,20 @@
-(function() {
+(function () {
     'use strict';
-    angular.module('blocitoff').controller('Main.controller', function($scope,$firebaseArray) {
-        var list = $firebaseArray(new Firebase("https://ng-blocitoff.firebaseio.com/"));
-        list.$add({done:false, text:'first'},{done:false, text:'second'}).then();
-        $scope.list = list;
-    });
+    angular.module('blocitoff').controller('Main.controller', ['$scope', '$firebaseArray', 'FBURL', function ($scope, $firebaseArray, FBURL) {
+        $scope.myData = new Firebase(FBURL);
+        $scope.todos = [];
+        $scope.addTodo = function () {
+            $scope.todos.push({
+                'title': $scope.newTodo,
+                'done': false
+            });
+            $scope.newTodo = '';
+            $scope.myData.push({todos:$scope.todos});
+        };
+        $scope.clearCompleted = function (item) {
+            $scope.todos = $scope.todos.filter(function(item) {
+                return !item.done;
+            });
+        };
+    }]);
 }());
