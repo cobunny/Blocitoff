db1cdabdc9fa426429da20daf8ae7114df69acc6
qiong <malatanggm@gmail.com>
2015-11-11 13:11:12 -0800

    Sync added new todos with firebase

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,7 +1,8 @@
 (function () {
     'use strict';
     angular.module('blocitoff').controller('Main.controller', ['$scope', '$firebaseArray', 'FBURL', function ($scope, $firebaseArray, FBURL) {
-        $scope.myData = new Firebase(FBURL);
+        var listRef = new Firebase(FBURL);
+        $scope.list = $firebaseArray(listRef);
         $scope.todos = [];
         $scope.addTodo = function () {
             $scope.todos.push({
@@ -9,10 +10,10 @@
                 'done': false
             });
             $scope.newTodo = '';
-            $scope.myData.push({todos:$scope.todos});
+            $scope.list.$add($scope.todos);
         };
         $scope.clearCompleted = function (item) {
-            $scope.todos = $scope.todos.filter(function(item) {
+            $scope.todos = $scope.todos.filter(function (item) {
                 return !item.done;
             });
         };
