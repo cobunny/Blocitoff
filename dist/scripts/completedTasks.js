9e36d18d31e78d509c9d694aa8caab3cbec194a0
qiong <malatanggm@gmail.com>
2015-11-16 10:11:54 -0800

    Added normalize.css and removed all h1s

--- dist/scripts/completedTasks.js
+++ dist/scripts/completedTasks.js
@@ -1,4 +1,6 @@
 (function() {
     'use strict';
-    angular.module('blocitoff').controller('CompletedTasks.controller', function($scope) {});
+    angular.module('blocitoff').controller('CompletedTasks.controller', function($scope) {
+        
+    });
 }());
